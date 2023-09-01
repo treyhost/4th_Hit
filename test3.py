8c34f5b20337b0ca26f80b59a10af0ab56323e12
treyhost <tre9020@gmail.com>
2023-09-01 10:34:17 -0500

    first commit

---
+++ test3.py
@@ -0,0 +1,11 @@
+for number in range(1, 101):
+    if number % 5 == 0:
+        print("Hit")
+    else:
+        print(number)
+x = 10  # Assign a value to x
+if x % 2 == 0:
+    print("Hit")
+if (x % 2 == 0):
+  print("Hit")
+
