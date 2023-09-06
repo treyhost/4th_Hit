2dce363444031c5be32890d306f56abcfadafbf4
treyhost <131905444+treyhost@users.noreply.github.com>
2023-09-06 08:30:28 -0700

    Update test3.py
    New passwords

--- test3.py
+++ test3.py
@@ -1,11 +1,12 @@
-for number in range(1, 101):
-    if number % 5 == 0:
-        print("Hit")
-    else:
-        print(number)
-x = 10  # Assign a value to x
-if x % 2 == 0:
-    print("Hit")
-if (x % 2 == 0):
-  print("Hit")
+import random
+import string
 
+def generate_random_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = 12  # Change this to set the desired length of the password
+    random_password = generate_random_password(password_length)
+    print("Your unique random password is:", random_password)
