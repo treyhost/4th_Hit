2c418fd46ea8608cabe7b3a824290a6623b47ffc
treyhost <131905444+treyhost@users.noreply.github.com>
2023-09-06 08:37:49 -0700

    Update test3.py
    Matrix Passwords

--- test3.py
+++ test3.py
@@ -1,12 +1,25 @@
 import random
 import string
 
-def generate_random_password(length):
+# List of character names from "The Matrix"
+matrix_characters = [
+    "Neo",
+    "Morpheus",
+    "Trinity",
+    "Agent Smith",
+    "Cypher",
+    "The Oracle",
+    "The Architect"
+]
+
+def generate_random_password(length, character_name):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
+    password = ''.join(random.choice(characters) for _ in range(length - len(character_name)))
+    password += character_name
     return password
 
 if __name__ == "__main__":
-    password_length = 12  # Change this to set the desired length of the password
-    random_password = generate_random_password(password_length)
-    print("Your unique random password is:", random_password)
+    password_length = 17  # Change this to set the desired length of the password (length of name + additional characters)
+    random_character_name = random.choice(matrix_characters)
+    random_password = generate_random_password(password_length, random_character_name)
+    print("Your unique random password with a 'Matrix' character name is:", random_password)
